df937493b51dbdd3ddf10742d9a01d3ac00af6a6
lbjworld <lbj.world@gmail.com>
2016-02-14 11:21:10 +0800

    fix demo bug

--- docker_demo/stage2/build.sh
+++ docker_demo/stage2/build.sh
@@ -2,5 +2,5 @@
 
 set -ue
 
-sudo docker --dns 208.67.220.220 --dns 208.67.222.222 build -t demo_stage2 .
+sudo docker build -t demo_stage2 .
 
--- docker_demo/stage2/dj_demo/entry-point.sh
+++ docker_demo/stage2/dj_demo/entry-point.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# input: tcp_addr, tcp_port
+wait_tcp_dependency()
+{
+    local tcp_addr=$1;
+    local tcp_port=$2;
+    local testing_url="tcp://${tcp_addr}:${tcp_port}"
+
+    # assign fd automatically
+    # refer to http://stackoverflow.com/questions/8295908/how-to-use-a-variable-to-indicate-a-file-descriptor-in-bash
+    while ! exec {id}<>/dev/tcp/${tcp_addr}/${tcp_port}; do
+        echo "$(date) - trying to connect to ${testing_url}"
+        sleep 1
+    done   
+}
+
+echo "connecting to db ..."
+wait_tcp_dependency ${DB_PORT_3306_TCP_ADDR} ${DB_PORT_3306_TCP_PORT}
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
